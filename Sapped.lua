fbaa7d225be1b2c84d89a50eeac4a910c7af0f75
Julian Lagerberg García <juliandreas@fastmail.com>
2024-03-20 18:23:56 +0100

    update to 10.2.6

--- Sapped.lua
+++ Sapped.lua
@@ -6,9 +6,7 @@ local frame = CreateFrame("Frame")
 local function shouldEnableAddon()
     local _, instanceType = GetInstanceInfo()
     local isPvpInstance = instanceType == "pvp" or instanceType == "arena"
-    local isWorldPvp = (instanceType == "none" and IsInActiveWorldPVP())
-    local hasWorldPvpEnabled = C_PvP.IsWarModeDesired() or UnitIsPVP("player")
-    return isPvpInstance or isWorldPvp or hasWorldPvpEnabled
+    return isPvpInstance
 end
 
 -- Update event registration based on conditions
@@ -22,7 +20,7 @@ end
 
 -- Message to send when the player is sapped
 local function SendMessage()
-    SendChatMessage(Sapped.GetMessage())
+    SendChatMessage(Sapped.GetMessage(), "SAY")
 end
 
 frame:SetScript("OnEvent", function(_, event, ...)
@@ -30,11 +28,13 @@ frame:SetScript("OnEvent", function(_, event, ...)
     if event == "COMBAT_LOG_EVENT_UNFILTERED" then
         -- Get the combat log event info
         local _, eventType, _, _, _, _, _, destGUID, _, _, _, spellId = CombatLogGetCurrentEventInfo()
-
         -- Check if the event is a debuff and the player is the target
         if (eventType == "SPELL_AURA_APPLIED" or eventType == "SPELL_AURA_REFRESH") and destGUID == UnitGUID("player") then
-            -- Check if the spell ID matches the "Sapped" debuff
-            if spellId == 6770 then
+            -- Check if the spell ID matches the "Sapped" debuff across different expansions
+                if spellId == 6770 -- Retail version of "Sapped"
+                or spellId == 2070 -- Classic version of "Sapped"
+                or spellId == 11297 -- Another Classic version
+                or spellId == 51724 then -- Wrath of the Lich King version of "Sapped", for example
                 SendMessage()
             end
         end
